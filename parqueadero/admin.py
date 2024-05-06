8dbece811e75a320792a5a8a04ac626aabe9be61
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-05-05 21:52:50 -0500

    Modelos

--- parqueadero/admin.py
+++ parqueadero/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
-from .models import usuario
+from .models import usuario, Sede ,Estacione, Punto, Facultade, Programa, Parkine
 
 class UserFields(admin.ModelAdmin):
-    list_display = ('email', 'password', 'status')
+    list_display = ('nombre','apellido','código','identificación','email', 'password', 'status')
+    
+class SedeFields(admin.ModelAdmin):
+    list_display = ('id_sede','nombre', 'dirección')
+    
+class EstaciónFields(admin.ModelAdmin):
+    list_display = ('id_estación', 'nombre', 'id_sede')
+    
+class PuntoFields(admin.ModelAdmin):
+    list_display = ('id_punto', 'id_estacion', 'estado','codigo_punto')
+    
+class FacultadFields(admin.ModelAdmin):
+    list_display = ('id_facultad', 'nombre', 'abrebiación','estado')
+    
+class ProgramaFields(admin.ModelAdmin):
+    list_display = ('id_programa', 'nombre', 'abrebiación', 'id_facultad')
+    
+class ParkineFields(admin.ModelAdmin):
+    list_display = ('id_parking', 'id_usurio', 'id_punto')
+    
 # Register your models here.
 admin.site.register(usuario, UserFields)
+admin.site.register(Sede, SedeFields)
+admin.site.register(Estacione, EstaciónFields)
+admin.site.register(Punto, PuntoFields)
+admin.site.register(Facultade, FacultadFields)
+admin.site.register(Programa, ProgramaFields)
+admin.site.register(Parkine, ParkineFields)
--- parqueadero/models.py
+++ parqueadero/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
-
 # Create your models here.
 
 class DateTimeModel(models.Model):
@@ -11,23 +10,37 @@ class DateTimeModel(models.Model):
     class Meta:
         abstract = True
         
-class Usuario(DateTimeModel):
+class usuario(DateTimeModel):
     id_usuario= models.AutoField(primary_key=True, unique=True, blank=False, null=False)
     nombre = models.CharField(max_length=50, blank=False, null=False)
     apellido = models.CharField(max_length=50, blank=False, null=False)
     código = models.CharField(max_length=50, blank=False, null=False)
     identificación = models.CharField(max_length=50, blank=False, null=False)
     email = models.EmailField(max_length=50, unique=True, blank=False, null=False)
-    contrasena = models.CharField(max_length=50, blank=False, null=False)
-    estado = models.BooleanField(default=True)
+    password = models.CharField(max_length=50, blank=False, null=False)
+    status = models.BooleanField(default=True)
     #jave foranea de programa 
-    id_programas = models.ForeignKey(Programa, on_delete=models.CASCADE, blank=False, null=False)
-
-class Programa(DateTimeModel):
-    id_programa = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
+    id_programas = models.ForeignKey('Programa', on_delete=models.CASCADE, blank=False, null=False)
+    
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)
+        super(usuario, self).save(*args, **kwargs)
+        
+class Sede(DateTimeModel):
+    id_sede = models.AutoField(primary_key=True, unique= True, blank= False, null=False)
     nombre = models.CharField(max_length=50, blank=False, null=False)
-    abrebiación = models.CharField(max_length=5, blank=False, null=False)
-    #id _facultad
+    dirección = models.CharField(max_length= 50, blank=False, null=False)
+    
+class Estacione(DateTimeModel):
+    id_estación = models.AutoField(primary_key=True, unique= True, blank= False, null=False)
+    nombre = models.CharField(max_length=50, blank=False, null=False)
+    id_sede = models.ForeignKey(Sede, on_delete=models.CASCADE, blank=False, null=False)
+    
+class Punto(DateTimeModel):
+    id_punto = models.AutoField(primary_key=True, unique= True, blank= False, null=False)
+    id_estacion = models.ForeignKey(Estacione, on_delete=models.CASCADE, blank=False, null=False)
+    estado = models.BooleanField(default=True)
+    codigo_punto = models.CharField(max_length=50, blank=False, null=False)
 
 class Facultade(DateTimeModel):
     id_facultad = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
@@ -35,23 +48,15 @@ class Facultade(DateTimeModel):
     abrebiación = models.CharField(max_length=5, blank=False, null=False)
     estado = models.BooleanField(default=True)
 
+class Programa(DateTimeModel):
+    id_programa = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
+    nombre = models.CharField(max_length=50, blank=False, null=False)
+    abrebiación = models.CharField(max_length=5, blank=False, null=False)
+    id_facultad = models.ForeignKey(Facultade, on_delete=models.CASCADE, blank=False, null=False)
+    
+
 class Parkine(DateTimeModel):
     id_parking = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
-    #id usuario
-    #id punto
+    id_usurio = models.ForeignKey(usuario, on_delete=models.CASCADE, blank=False, null=False)
+    id_punto = models.ForeignKey(Punto, on_delete=models.CASCADE, blank=False, null=False) 
     
-class Estacione(DateTimeModel):
-    id_estación = models.AutoField(primary_key=True, unique= True, blank= False, null=False)
-    nombre = models.CharField(max_length=50, blank=False, null=False)
-    #id sede
-    
-class Punto(DateTimeModel):
-    id_punto = models.AutoField(primary_key=True, unique= True, blank= False, null=False)
-    #id_estacion
-    estado = models.BooleanField(default=True)
-    codigo_punto = models.AutoField(primary_key=True, unique= True, blank= False, null=False)
-    
-class Sede(DateTimeModel):
-    id_sede = models.AutoField(primary_key=True, unique= True, blank= False, null=False)
-    nombre = models.CharField(max_length=50, blank=False, null=False)
-    dirección = models.CharField(max_length= 50, blank=False, null=False)
