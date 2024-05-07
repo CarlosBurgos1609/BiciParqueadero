6f72522661268ffccf227a5d6daa591ebdf5af4b
Julian <julianalvarez199423@gmail.com>
2024-05-07 16:15:47 -0500

    CESMAG

--- parqueadero/urls.py
+++ parqueadero/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.index, name='index'),
     path("usuario",views.lista_usurio, name='usuario'),
     path("login",views.login, name='login'),
+    path("register",views.register, name='register'),
     # path("transacciones",views.lista_transacciones, name='transacciones')
     
 ]
--- parqueadero/views.py
+++ parqueadero/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import usuario
+from .models import Sede
 
 def index(request):
     return HttpResponse(":::Welcom to my site:::")
@@ -12,6 +13,10 @@ def lista_usurio(request):
     return render(request, 'parqueadero/user.html', {'usuario':usuarios})
 
 def login(request):
-    #return HttpResponse ("Here you find a list of users")
-    return render(request, 'parqueadero/login.html',)
+    #return HttpResponse ("Here you find a list of users"
+    return render(request, 'parqueadero/login.html')
 
+def register(request):
+    #return HttpResponse ("Here you find a list of users")
+    sedes = Sede.objects,all()
+    return render(request, 'parqueadero/register.html',{'instituto':sedes})
