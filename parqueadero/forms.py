7b0244f328970c1a8112bdd92fd919d8e869905e
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-05-15 21:49:29 -0500

    update

--- parqueadero/forms.py
+++ parqueadero/forms.py
@@ -12,7 +12,7 @@ class usuariosForm(forms.Form):
     apellido = forms.CharField(label="apellido",max_length=150, required=True)
     codigo = forms.CharField(label="codigo",max_length=11, required=True)
     identificacion = forms.CharField(label="identificacion",max_length=11, required=True)
-    email = forms.CharField(label="email",max_length=200, required=True)
+    email = forms.EmailField(label="email",max_length=200, required=True)
     password =forms.CharField(label="contraseña",max_length=250, required=True, widget=forms.PasswordInput)
     #password =forms.CharField(label="contraseña",max_length=250,validators=[password_validator], required=True, widget=forms.PasswordInput)
     
