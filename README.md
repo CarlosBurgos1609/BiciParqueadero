c1d1916ef0c38b98a4f25f74e15115abd50d472f
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-05-17 01:38:42 -0500

    update

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-# BiciParqueadero
-Proyecto de inter-estructurante de bici parqueadero
+## Django Template
 
----
--`.env`
-```python
-DB_NAME=BiciParqueadero
-DB_USER=postgres
-DB_PASSWORD=1234
-DB_HOST=localhost
-DB_PORT=5432
-```
-
-
+- py -m venv venv
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for BiciParqueadero project.
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -20,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("parqueadero.urls")),
-    #path('accounts/', include('django.contrib.auth.urls')),
 ]
